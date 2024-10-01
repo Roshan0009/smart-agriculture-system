d2ec189ecca1429db23cdfdf5238e760d570dc42
Roshan0009 <roshanlamichhane47@gmail.com>
2024-09-30 22:38:36 -0400

    Initial commit of Smart Agriculture project

--- dist/cropMonitoring/cropMonitoring.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.monitorCropHealth = monitorCropHealth;
-function monitorCropHealth() {
-    console.log("Monitoring crop health...");
-}
--- dist/irrigation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// src/irrigation.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.manageIrrigation = manageIrrigation;
-function manageIrrigation(crop) {
-    console.log(`Irrigation started for ${crop}`);
-    // Add any additional logic for irrigation management here
-}
